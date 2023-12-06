b602abc975253a226308b628e3c787c03902cd69
Luigi-Iorio <luigi0iorio@gmail.com>
2023-12-06 15:47:47 +0100

    feat: componente titolo

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,9 +1,15 @@
 <script>
-
+export default{
+    data(){
+        return{
+            title: "La mia prima app con Vite!"
+        }
+    },
+}
 </script>
 
 <template>
-
+    <h1>{{ title }}</h1>
 </template>
 
 <style scoped>
