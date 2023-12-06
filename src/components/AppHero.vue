68bb1ee23c9543442361127150accb4a1f18b827
Luigi-Iorio <luigi0iorio@gmail.com>
2023-12-06 16:01:20 +0100

    feat: componente AppHero

---
+++ src/components/AppHero.vue
@@ -0,0 +1,29 @@
+<script>
+export default{
+    data(){
+        return{
+            text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit sequi et repudiandae nam vero corporis error obcaecati tempore voluptates ex doloremque perferendis itaque soluta ratione omnis iste, excepturi minima consectetur."
+        }
+    }
+}
+</script>
+
+<template>
+    <p>{{ text }}</p>
+    <button>Button</button>
+</template>
+
+<style scoped>
+button{
+    padding: 10px 20px;
+    background-color: rgb(11, 109, 77);
+    border: none;
+    border-radius: 10px;
+    transition: 0.4s;
+}
+
+button:hover{
+    cursor: pointer;
+    background-color: rgb(8, 72, 51);
+}
+</style>
